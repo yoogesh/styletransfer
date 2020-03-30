85e98f085140b3199c76108c09801e0344450edd
ramyoogesh <yoogie.ram@gmail.com>
2020-03-30 16:25:23 -0400

    Lec 1 Completed

--- styletransfer/ViewController.swift
+++ styletransfer/ViewController.swift
@@ -6,15 +6,43 @@
 //  Copyright © 2020 Ram Yoogesh . All rights reserved.
 //
 
-import UIKit
+import UIKit   //ios framework whihc has butttons and all he stuff
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // put the methods and variables here
+    
+    var flipcount : Int  =  0 {
+        didSet {
+            FlipcountLabel.text = " Flips : \(flipcount)"
+        }
     }
-
-
+    
+    // Creating an array of buttons
+    // [UIButton] specifies an array of type button
+    @IBOutlet var Cardbuttons: [UIButton]!
+    
+    // Defining an label with an optional
+    @IBOutlet weak var FlipcountLabel: UILabel!
+    
+    var emojiarray: Array<String> = ["🎃 ", "👻 ", "👻 ", " 🎃"]
+     
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipcount += 1
+        let cardnumber = Cardbuttons.firstIndex(of: sender)!
+        print("cardnumber = \(String(describing: cardnumber))")
+        Flipcard(withEmoji: emojiarray[cardnumber], on: sender)
+    }
+    func Flipcard (withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for:  UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji,  for:  UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
 
